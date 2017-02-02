b8f86296e629e330b7c5426aa580e0b932e3ab69
Zac King <ZacKingx@gmail.com>
2017-02-02 09:18:22 -0600

    changing spawn options

--- Assets/Scripts/SpawnBalls.cs
+++ Assets/Scripts/SpawnBalls.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class SpawnBalls : MonoBehaviour
 {
-    [SerializeField]
-    GameObject m_BouncyBallPrefab;
-    SteamVR_TrackedController thisController;
+    [SerializeField] GameObject m_BallPrefab;
+    [SerializeField] GameObject m_storedPrefab1;
+    [SerializeField] GameObject m_storedPrefab2;
+    [SerializeField] GameObject m_storedPrefab3;
+
+    private SteamVR_TrackedController thisController;
     private bool touchingball = false;
 
     private void Start()
@@ -17,11 +20,29 @@ public class SpawnBalls : MonoBehaviour
     {
 	    if(thisController.triggerPressed && !touchingball)
         {
-            GameObject g = Instantiate(m_BouncyBallPrefab);
+            GameObject g = Instantiate(m_BallPrefab);
             g.transform.position = transform.position;
 
         }
 	}
+
+    public void ChangeSpawnObject(int index)
+    {
+        switch(index)
+        {
+            case 1:
+                m_BallPrefab = m_storedPrefab1;
+                break;
+            case 2:
+                m_BallPrefab = m_storedPrefab2;
+                break;
+            case 3:
+                m_BallPrefab = m_storedPrefab3;
+                break;
+            default:
+                break;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
